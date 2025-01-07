9b5ed9da5c9cadf7b7235f7778c086519e6623a7
gilang <gilang.sukma@digitalsekuriti.id>
2025-01-08 02:57:26 +0700

    update

--- api/monitoring.php
+++ api/monitoring.php
@@ -1,98 +1,31 @@
 <?php
-header('Content-Type: application/json');
 
-// Include database connection
 include './config.php';
 
+
+header('Content-Type: application/json');
+
+// Get the PDO instance
+global $pdo;
+
 try {
-    // Get the content type of the request
-    $contentType = $_SERVER['CONTENT_TYPE'];
-    $reqBody = strtolower($contentType);
 
-    // Initialize data array
-    $data = [];
 
-    if (strpos($reqBody, 'form-data') !== false || strpos($reqBody, 'x-www-form-urlencoded') !== false) {
-        // Get data from form-data or x-www-form-urlencoded
-        $data = [
-            'temperature' => isset($_POST['temperature']) ? $_POST['temperature'] : null,
-            'pln_volt' => isset($_POST['pln_volt']) ? $_POST['pln_volt'] : null,
-            'pln_current' => isset($_POST['pln_current']) ? $_POST['pln_current'] : null,
-            'pln_activity' => isset($_POST['pln_activity']) ? $_POST['pln_activity'] : null,
-            'pln_status' => isset($_POST['pln_status']) ? $_POST['pln_status'] : null,
-            'accu_volt' => isset($_POST['accu_volt']) ? $_POST['accu_volt'] : null,
-            'accu_current' => isset($_POST['accu_current']) ? $_POST['accu_current'] : null,
-            'accu_activity' => isset($_POST['accu_activity']) ? $_POST['accu_activity'] : null,
-            'accu_status' => isset($_POST['accu_status']) ? $_POST['accu_status'] : null,
-            //'ups_volt' => isset($_POST['ups_volt']) ? $_POST['ups_volt'] : null,
-            //'ups_current' => isset($_POST['ups_current']) ? $_POST['ups_current'] : null,
-            //'ups_activity' => isset($_POST['ups_activity']) ? $_POST['ups_activity'] : null,
-            //'ups_status' => isset($_POST['ups_status']) ? $_POST['ups_status'] : null,
-            'device_id' => isset($_POST['device_id']) ? $_POST['device_id'] : null,
-            'soc' => isset($_POST['soc']) ? $_POST['soc'] : null,
-            'accu_estimate_time' => isset($_POST['accu_estimate_time']) ? $_POST['accu_estimate_time'] : null,
-            'accu_info' => isset($_POST['accu_info']) ? $_POST['accu_info'] : null
-        ];
-    } else {
-        // Unsupported content type
-        http_response_code(400);
-        echo json_encode(['message' => 'Unsupported Content-Type']);
-        exit;
-    }
-
-    // Validate required fields
-    $requiredFields = [
-        'temperature', 'pln_volt', 'pln_current', 'pln_activity', 'pln_status',
-        'accu_volt', 'accu_current', 'accu_activity', 'accu_status',
-        'device_id', 'soc', 'accu_estimate_time', 'accu_info'
+    $response = [
+        "device" => []
     ];
-    foreach ($requiredFields as $field) {
-        if (!isset($data[$field]) || empty($data[$field])) {
-            http_response_code(400);
-            echo json_encode(['message' => "Missing required field: $field"]);
-            exit;
-        }
-    }
 
-    // Prepare SQL query
-    $stmt = $pdo->prepare("
-        INSERT INTO log (
-            temperature, pln_volt, pln_current, pln_activity, pln_status,
-            accu_volt, accu_current, accu_activity, accu_status,
-            device_id, soc, accu_estimate_time, accu_info
-        ) VALUES (
-            :temperature, :pln_volt, :pln_current, :pln_activity, :pln_status,
-            :accu_volt, :accu_current, :accu_activity, :accu_status,
-            :device_id, :soc, :accu_estimate_time, :accu_info
-        )
-    ");
+    // Table data
+    // Fetch latest data
+    $q = "";
+    $stmt = $pdo->prepare($q);
+    $stmt->execute();
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Bind parameters and execute query
-    $stmt->execute([
-        ':temperature' => $data['temperature'],
-        ':pln_volt' => $data['pln_volt'],
-        ':pln_current' => $data['pln_current'],
-        ':pln_activity' => $data['pln_activity'],
-        ':pln_status' => $data['pln_status'],
-        ':accu_volt' => $data['accu_volt'],
-        ':accu_current' => $data['accu_current'],
-        ':accu_activity' => $data['accu_activity'],
-        ':accu_status' => $data['accu_status'],
-        // ':ups_volt' => $data['ups_volt'],
-        //':ups_current' => $data['ups_current'],
-        //':ups_activity' => $data['ups_activity'],
-        //':ups_status' => $data['ups_status'],
-        ':device_id' => $data['device_id'],
-        ':soc' => $data['soc'],
-        ':accu_estimate_time' => $data['accu_estimate_time'],
-        ':accu_info' => $data['accu_info']
-    ]);
-
-    // Return OK response
-    http_response_code(200);
-    echo json_encode(['message' => 'OK']);
-} catch (Exception $e) {
-    // Handle exceptions
-    http_response_code(500);
-    echo json_encode(['message' => 'Error occurred', 'error' => $e->getMessage()]);
+    error_log(json_encode($data));
+    // Return the JSON response
+    echo json_encode($data);
+} catch (PDOException $e) {
+    // Return error message
+    echo json_encode(['error' => $e->getMessage(), 'status' => 'failed']);
 }
--- api/receive_logs.php
+++ api/receive_logs.php
@@ -12,31 +12,27 @@ try {
     // Initialize data array
     $data = [];
 
-    if (strpos($reqBody, 'form-data') !== false) {
-        // Get data from form-data
+    if (strpos($reqBody, 'form-data') !== false || strpos($reqBody, 'x-www-form-urlencoded') !== false) {
+        // Get data from form-data or x-www-form-urlencoded
         $data = [
-            'temperature' => $_POST['temperature'],
-            'pln_volt' => $_POST['pln_volt'],
-            'pln_current' => $_POST['pln_current'],
-            'pln_activity' => $_POST['pln_activity'],
-            'pln_status' => $_POST['pln_status'],
-            'accu_volt' => $_POST['accu_volt'],
-            'accu_current' => $_POST['accu_current'],
-            'accu_activity' => $_POST['accu_activity'],
-            'accu_status' => $_POST['accu_status'],
-            'ups_volt' => $_POST['ups_volt'],
-            'ups_current' => $_POST['ups_current'],
-            'ups_activity' => $_POST['ups_activity'],
-            'ups_status' => $_POST['ups_status'],
-            'device_id' => $_POST['device_id'],
-            'soc' => $_POST['soc'],
-            'accu_estimate_time' => $_POST['accu_estimate_time'],
-            'accu_info' => $_POST['accu_info']
+            'temperature' => isset($_POST['temperature']) ? $_POST['temperature'] : null,
+            'pln_volt' => isset($_POST['pln_volt']) ? $_POST['pln_volt'] : null,
+            'pln_current' => isset($_POST['pln_current']) ? $_POST['pln_current'] : null,
+            'pln_activity' => isset($_POST['pln_activity']) ? $_POST['pln_activity'] : null,
+            'pln_status' => isset($_POST['pln_status']) ? $_POST['pln_status'] : null,
+            'accu_volt' => isset($_POST['accu_volt']) ? $_POST['accu_volt'] : null,
+            'accu_current' => isset($_POST['accu_current']) ? $_POST['accu_current'] : null,
+            'accu_activity' => isset($_POST['accu_activity']) ? $_POST['accu_activity'] : null,
+            'accu_status' => isset($_POST['accu_status']) ? $_POST['accu_status'] : null,
+            //'ups_volt' => isset($_POST['ups_volt']) ? $_POST['ups_volt'] : null,
+            //'ups_current' => isset($_POST['ups_current']) ? $_POST['ups_current'] : null,
+            //'ups_activity' => isset($_POST['ups_activity']) ? $_POST['ups_activity'] : null,
+            //'ups_status' => isset($_POST['ups_status']) ? $_POST['ups_status'] : null,
+            'device_id' => isset($_POST['device_id']) ? $_POST['device_id'] : null,
+            'soc' => isset($_POST['soc']) ? $_POST['soc'] : null,
+            'accu_estimate_time' => isset($_POST['accu_estimate_time']) ? $_POST['accu_estimate_time'] : null,
+            'accu_info' => isset($_POST['accu_info']) ? $_POST['accu_info'] : null
         ];
-    } elseif (strpos($reqBody, 'json') !== false) {
-        // Get data from JSON body
-        $json = file_get_contents('php://input');
-        $data = json_decode($json, true);
     } else {
         // Unsupported content type
         http_response_code(400);
@@ -48,11 +44,10 @@ try {
     $requiredFields = [
         'temperature', 'pln_volt', 'pln_current', 'pln_activity', 'pln_status',
         'accu_volt', 'accu_current', 'accu_activity', 'accu_status',
-        'ups_volt', 'ups_current', 'ups_activity', 'ups_status',
         'device_id', 'soc', 'accu_estimate_time', 'accu_info'
     ];
     foreach ($requiredFields as $field) {
-        if (!isset($data[$field])) {
+        if (!isset($data[$field]) || empty($data[$field])) {
             http_response_code(400);
             echo json_encode(['message' => "Missing required field: $field"]);
             exit;
@@ -64,12 +59,10 @@ try {
         INSERT INTO log (
             temperature, pln_volt, pln_current, pln_activity, pln_status,
             accu_volt, accu_current, accu_activity, accu_status,
-            ups_volt, ups_current, ups_activity, ups_status,
             device_id, soc, accu_estimate_time, accu_info
         ) VALUES (
             :temperature, :pln_volt, :pln_current, :pln_activity, :pln_status,
             :accu_volt, :accu_current, :accu_activity, :accu_status,
-            :ups_volt, :ups_current, :ups_activity, :ups_status,
             :device_id, :soc, :accu_estimate_time, :accu_info
         )
     ");
@@ -85,10 +78,10 @@ try {
         ':accu_current' => $data['accu_current'],
         ':accu_activity' => $data['accu_activity'],
         ':accu_status' => $data['accu_status'],
-        ':ups_volt' => $data['ups_volt'],
-        ':ups_current' => $data['ups_current'],
-        ':ups_activity' => $data['ups_activity'],
-        ':ups_status' => $data['ups_status'],
+        // ':ups_volt' => $data['ups_volt'],
+        //':ups_current' => $data['ups_current'],
+        //':ups_activity' => $data['ups_activity'],
+        //':ups_status' => $data['ups_status'],
         ':device_id' => $data['device_id'],
         ':soc' => $data['soc'],
         ':accu_estimate_time' => $data['accu_estimate_time'],
